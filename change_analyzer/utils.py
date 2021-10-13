563e4d03cca73bca6aba4a34adbfdb2e6ba2d8d1
Matvey Pashkovskiy <matvey.pashkovskiy@f-secure.com>
2021-10-13 08:58:15 +0300

    feat: iou calculation for elements

--- change_analyzer/utils.py
+++ change_analyzer/utils.py
@@ -1,6 +1,7 @@
-from typing import Tuple
+from typing import Tuple, Dict
 
 from PIL import Image, ImageOps
+from shapely.geometry import Polygon
 
 
 def image_pad_resize_to(im: Image, new_dims: Tuple[int, int]) -> Image:
@@ -17,3 +18,21 @@ def image_pad_resize_to(im: Image, new_dims: Tuple[int, int]) -> Image:
 
     padding = (round(delta_w / 2), round(delta_h / 2))
     return ImageOps.expand(im, padding, "black").resize(new_dims, Image.ANTIALIAS)
+
+
+def iou(rect0: Dict[str, int], rect1: Dict[str, int]) -> float:
+    # self._logger.info(f"checking rects: {el0.rect}, {el1.rect}")
+    # https://stackoverflow.com/a/42874377 or more compact: https://stackoverflow.com/a/57247833
+    poly0 = Polygon([
+        [rect0["x"], rect0["y"]],
+        [rect0["x"] + rect0["width"], rect0["y"]],
+        [rect0["x"] + rect0["width"], rect0["y"] + rect0["height"]],
+        [rect0["x"], rect0["y"] + rect0["height"]]
+    ])
+    poly1 = Polygon([
+        [rect1["x"], rect1["y"]],
+        [rect1["x"] + rect1["width"], rect1["y"]],
+        [rect1["x"] + rect1["width"], rect1["y"] + rect1["height"]],
+        [rect1["x"], rect1["y"] + rect1["height"]]
+    ])
+    return poly0.intersection(poly1).area / poly0.union(poly1).area
