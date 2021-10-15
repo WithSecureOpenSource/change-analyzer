e812a20843c8f00c4e951e8ea4042dcb6aa6ee1a
Joona Oikarinen <joona.oikarinen@f-secure.com>
2021-10-15 15:26:05 +0300

    test: add pytest to publish requirement
    Add pytest to publish action.
    Make test compatible with headless environment.

--- change_analyzer/tests/test_utils.py
+++ change_analyzer/tests/test_utils.py
@@ -1,12 +1,14 @@
-from PIL import ImageGrab
+from PIL import Image
+import os
 
 from change_analyzer.utils import image_pad_resize_to
 
 
 def test_image_pad_resize_to_wide():
-    size = (800, 400)
-    im = image_pad_resize_to(ImageGrab.grab(), size)
-    im.show()
+    size = (200, 200)
+    cwd = os.getcwd()
+    image_from_disk = Image.open(f"{cwd}/change_analyzer/tests/400x400.png")
+    im = image_pad_resize_to(image_from_disk, size)
     assert im.size == size
 
 
