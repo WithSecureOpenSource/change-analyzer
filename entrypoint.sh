f1dae916907e91ef3c5399088d2f9030ad5a7db3
Joona Oikarinen <joona.oikarinen@f-secure.com>
2021-11-04 11:12:26 +0200

    test: docker generate results
    Docker parameters changed to generate data.
    Result parsing bug fixed.

--- entrypoint.sh
+++ entrypoint.sh
@@ -48,7 +48,7 @@ cat input.ini
 if [ "$GENERATE" = "1" ]; then
     echo "GENERATE"
     ca-run --config input.ini
-    FIRST_RECORD=$(ls -l recordings|tail -n1|cut --delimiter=' ' -f9)
+    FIRST_RECORD=$(ls -l recordings|tail -n1|tr -s " "|cut --delimiter=' ' -f9)
 fi
 
 # Replay
@@ -56,15 +56,15 @@ if [ "$REPLAY" = "1" ] && [ "$CSV" = "1" ]; then
     echo "REPLAY IMPORTED CSV"
     mkdir -p /home/localtester/change-analyzer/recordings
     cp -r "$CSV_PATH"/recordings /home/localtester/change-analyzer/
-    FIRST_RECORD=$(ls -l recordings|tail -n1|cut --delimiter=' ' -f9)
+    FIRST_RECORD=$(ls -l recordings|tail -n1|tr -s " "|cut --delimiter=' ' -f9)
     ca-run --config input.ini --csv_folder=recordings/"$FIRST_RECORD"
-    SECOND_RECORD=$(ls -l recordings|tail -n1|cut --delimiter=' ' -f9)
+    SECOND_RECORD=$(ls -l recordings|tail -n1|tr -s " "|cut --delimiter=' ' -f9)
     echo "$SECOND_RECORD" > /home/localtester/next_result.txt
 elif [ "$REPLAY" = "1" ]; then
     echo "REPLAY"
-    FIRST_RECORD=$(ls -l recordings|tail -n1|cut --delimiter=' ' -f9)
+    FIRST_RECORD=$(ls -l recordings|tail -n1|tr -s " "|cut --delimiter=' ' -f9)
     ca-run --config input.ini --csv_folder=recordings/"$FIRST_RECORD"
-    SECOND_RECORD=$(ls -l recordings|tail -n1|cut --delimiter=' ' -f9)
+    SECOND_RECORD=$(ls -l recordings|tail -n1|tr -s " "|cut --delimiter=' ' -f9)
     echo "$SECOND_RECORD" > /home/localtester/next_result.txt
 fi
 
